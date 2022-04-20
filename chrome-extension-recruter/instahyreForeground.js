52594703e340002146656cf9b5a8bea0546fa063
Yatin <yatinkathuria74@gmail.com>
2022-04-20 10:04:51 +0530

    add etension on instahyre

--- chrome-extension-recruter/instahyreForeground.js
+++ chrome-extension-recruter/instahyreForeground.js
@@ -1,47 +1,76 @@
 console.log('Inside  myInstahyreScript content script');
 
 const getContentJson = async (model) => {
-    debugger;
-    const name = $(modal[0]).find('.candidate-name span')[0].innerHTML
-    const gitHub = $(modal[0]).find('.social-github a')[0].href;
-    const linkedin = $(modal[0]).find('.social-linkedin a')[0].href;
-    const PhoneNumber = $($(modal[0]).find('.info')[5]).find('span')[0].innerHTML;
-    const email = $($(modal[0]).find('.info')[4]).find('span')[0].innerHTML;
-    const linkedInProfile = linkedin.split("/");
-    const data = await getDetailsByAny({
-        name,gitHub,linkedInProfile,email
-    });
-    return data;
-  };
+  // debugger;
+  const name = $(model[0]).find('.candidate-name span')[0].innerHTML;
+  // const gitHub = $(model[0]).find('.social-github a')[0].href;
+  // const linkedin = $(model[0]).find('.social-linkedin a')[0].href;
+  const PhoneNumber = $($(model[0]).find('.info')[5]).find('span')[0].innerHTML;
+  const email = $($(model[0]).find('.info')[4]).find('span')[0].innerHTML;
+  // const linkedInProfile = linkedin.split('/');
+  const data = { name, PhoneNumber, email };
+  // const data = await getDetailsByAny({
+  //   name,
+  //   gitHub,
+  //   linkedInProfile,
+  //   email,
+  // });
+  console.log(data);
+  return data;
+};
+
+const mainPopup = `<div id="demo-modal-popup" class="my_modal">
+	<div class="modal__content">
+		<div id='iframe-wrapper'>
+		</div>
+		<a href="#" class="modal__close">&times;</a>
+	</div>
+</div>`;
+
+function hideModal() {
+  $('#demo-modal-popup').removeClass('modal__target');
+  $('#iframe-wrapper').html('');
+}
+
+function showModal(candidateInfo) {
+  var iframe = document.createElement('iframe');
+  document.getElementById('iframe-wrapper').appendChild(iframe);
+  iframe.src = `https://sleepy-meadow-81233.herokuapp.com/?find=yatinkathuria74@gmail.com`;
+  $('#demo-modal-popup').addClass('modal__target');
+  $('#demo-modal-popup').click(hideModal);
+}
+
+function handleThinkifyButtonClick(event) {
+  event.stopPropagation();
+  showModal('amangautam72');
+}
+
+function addButtonIfCorrectPage() {
+  const linksContainer = document.querySelector('.candidate-social-links');
+  const span = document.createElement('span');
+  span.classList.add('thinkify-info-button');
+  span.textContent = 'T';
+  linksContainer.append(span);
+  span.addEventListener('click', handleThinkifyButtonClick);
+
+  setTimeout(() => {
+    $('body').append(mainPopup);
+  }, 0);
+}
 
 // A $( document ).ready() block.
-$( document ).ready(function() {
-    console.log( "ready!" );
-    $('.button-download-resume').click(function(e) {
-        console.log('onclick is called');
-        e.stopPropagation();
-        console.log('$( this )',$( this ));
-        var checkExist = setInterval(function() {
-            if ($('.application-modal-wrap .social-link.social-linkedin.ng-scope a').length) {
-               console.log("Exists!");
-                const model = $('.application-modal-wrap')
-                getContentJson(model).then((candidate) => {
-                    console.log("candidate:", candidate);
-                    const $newdiv1 = $(`<div id='layer'> 
-                        <div>Current CTC: ${candidate.currentSalary}</div>
-                        <div>Expected CTC: ${candidate.expectedSalary}</div>
-                        <div>Notice period :${candidate.noticePeriod}</div>
-                        <div>Updated on :${candidate.date}</div>
-                        </div>`),
-                      newdiv2 = document.createElement("div"),
-                      existingdiv1 = document.getElementById("foo");
-                      model.append( $newdiv1, [ newdiv2, existingdiv1 ] );
+$(document).ready(function () {
+  console.log('ready!');
+  $('.button-download-resume').click(function (e) {
+    e.stopPropagation();
+    const id = setInterval(function () {
+      if ($('.application-modal-wrap').length) {
+        const model = $('.application-modal-wrap');
+        getContentJson(model);
+        addButtonIfCorrectPage();
 
-                  });
-
-               clearInterval(checkExist);
-            }
-         }, 1000);
-    
-    });
+        clearInterval(id);
+      }
+    }, 500);
+  });
 });

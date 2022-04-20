731117710c93fdd30bccdfa05b00feaf8f0878c0
Yatin <yatinkathuria74@gmail.com>
2022-04-20 20:03:36 +0530

    change button position

--- chrome-extension-recruter/instahyreForeground.js
+++ chrome-extension-recruter/instahyreForeground.js
@@ -1,12 +1,12 @@
-console.log('Inside  myInstahyreScript content script');
+console.log("Inside  myInstahyreScript content script");
 
 const getContentJson = async (model) => {
   // debugger;
-  const name = $(model[0]).find('.candidate-name span')[0].innerHTML;
+  const name = $(model[0]).find(".candidate-name span")[0].innerHTML;
   // const gitHub = $(model[0]).find('.social-github a')[0].href;
   // const linkedin = $(model[0]).find('.social-linkedin a')[0].href;
-  const PhoneNumber = $($(model[0]).find('.info')[5]).find('span')[0].innerHTML;
-  const email = $($(model[0]).find('.info')[4]).find('span')[0].innerHTML;
+  const PhoneNumber = $($(model[0]).find(".info")[5]).find("span")[0].innerHTML;
+  const email = $($(model[0]).find(".info")[4]).find("span")[0].innerHTML;
   // const linkedInProfile = linkedin.split('/');
   const data = { name, PhoneNumber, email };
   // const data = await getDetailsByAny({
@@ -19,6 +19,15 @@ const getContentJson = async (model) => {
   return data;
 };
 
+const button = `<div class="info-button" id="thinkify-button">
+<label>
+    <a class="button-download-resume button-download cursor-pointer ng-scope" target="_blank" href="">
+      <i class="fa fa-fw fa-info"></i>Show Info
+    </a>
+</label>
+<span class="action-checked ng-scope" tooltip=""></span>
+</div>`;
+
 const mainPopup = `<div id="demo-modal-popup" class="my_modal">
 	<div class="modal__content">
 		<div id='iframe-wrapper'>
@@ -28,44 +37,44 @@ const mainPopup = `<div id="demo-modal-popup" class="my_modal">
 </div>`;
 
 function hideModal() {
-  $('#demo-modal-popup').removeClass('modal__target');
-  $('#iframe-wrapper').html('');
+  $("#demo-modal-popup").removeClass("modal__target");
+  $("#iframe-wrapper").html("");
 }
 
 function showModal(candidateInfo) {
-  var iframe = document.createElement('iframe');
-  document.getElementById('iframe-wrapper').appendChild(iframe);
+  var iframe = document.createElement("iframe");
+  document.getElementById("iframe-wrapper").appendChild(iframe);
   iframe.src = `https://sleepy-meadow-81233.herokuapp.com/?find=yatinkathuria74@gmail.com`;
-  $('#demo-modal-popup').addClass('modal__target');
-  $('#demo-modal-popup').click(hideModal);
+  $("#demo-modal-popup").addClass("modal__target");
+  $("#demo-modal-popup").click(hideModal);
 }
 
 function handleThinkifyButtonClick(event) {
+  event.preventDefault();
   event.stopPropagation();
-  showModal('amangautam72');
+  showModal("amangautam72");
 }
 
 function addButtonIfCorrectPage() {
-  const linksContainer = document.querySelector('.candidate-social-links');
-  const span = document.createElement('span');
-  span.classList.add('thinkify-info-button');
-  span.textContent = 'T';
-  linksContainer.append(span);
-  span.addEventListener('click', handleThinkifyButtonClick);
+  console.log("====in buton ciode");
+  $(".candidate-action-links.resume-modal > div:nth-child(" + 3 + ")").after(
+    button
+  );
 
   setTimeout(() => {
-    $('body').append(mainPopup);
+    $("#thinkify-button").click(handleThinkifyButtonClick);
+    $("body").append(mainPopup);
   }, 0);
 }
 
 // A $( document ).ready() block.
 $(document).ready(function () {
-  console.log('ready!');
-  $('.button-download-resume').click(function (e) {
+  console.log("ready!");
+  $(".button-download-resume").click(function (e) {
     e.stopPropagation();
     const id = setInterval(function () {
-      if ($('.application-modal-wrap').length) {
-        const model = $('.application-modal-wrap');
+      if ($(".application-modal-wrap").length) {
+        const model = $(".application-modal-wrap");
         getContentJson(model);
         addButtonIfCorrectPage();
 

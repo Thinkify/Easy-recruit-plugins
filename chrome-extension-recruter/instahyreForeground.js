80c430149d33104f36b4b6ac186ca6c66acfaaea
Yatin <yatinkathuria74@gmail.com>
2022-04-25 11:14:49 +0530

    fix current salary bug

--- chrome-extension-recruter/instahyreForeground.js
+++ chrome-extension-recruter/instahyreForeground.js
@@ -1,32 +1,33 @@
-console.log("Inside  myInstahyreScript content script");
+console.log('Inside  myInstahyreScript content script');
 
 const getContentJson = (model) => {
   if (!model.length) {
     return null;
   }
 
-  const name = $(model[0]).find(".candidate-name span")[0]?.innerHTML;
-  const email = $($(model[0]).find(".info")[4]).find("a")[0]?.innerHTML;
-  let currentSalary = $($(model[0]).find(".info")[3]).find("span")[1]
+  const name = $(model[0]).find('.candidate-name span')[0]?.innerHTML;
+  const email = $($(model[0]).find('.info')[4]).find('a')[0]?.innerHTML;
+  let currentSalary = $($(model[0]).find('.info')[3]).find('span')[1]
     ?.innerHTML;
   console.log(currentSalary);
   if (currentSalary) {
-    currentSalary = currentSalary.split(" ")[0];
+    currentSalary = currentSalary.split(' ')[0];
+    currentSalary = Number(currentSalary) * 100000; // 1 Lakh
   }
 
-  let gitHub = $(model[0]).find(".social-github a")[0]?.href;
+  let gitHub = $(model[0]).find('.social-github a')[0]?.href;
   if (gitHub) {
-    gitHub = gitHub?.split("/")[3];
+    gitHub = gitHub?.split('/')[3];
   }
 
-  let linkedInProfile = $(model[0]).find(".social-linkedin a")[0]?.href;
+  let linkedInProfile = $(model[0]).find('.social-linkedin a')[0]?.href;
   if (linkedInProfile) {
-    linkedInProfile = linkedInProfile.split("/")[4];
+    linkedInProfile = linkedInProfile.split('/')[4];
   }
 
-  let contact = $($(model[0]).find(".info")[5]).find("span")[0]?.innerHTML;
+  let contact = $($(model[0]).find('.info')[5]).find('span')[0]?.innerHTML;
   if (contact) {
-    contact = contact.split("/")[0];
+    contact = contact.split('/')[0];
   }
 
   const data = {
@@ -61,61 +62,63 @@ const popupSidebar = `
 </div>`;
 
 async function openSidebar(event) {
+  // const URL = 'https://sleepy-meadow-81233.herokuapp.com';
+  const URL = 'http://localhost:3000';
   event.preventDefault();
   event.stopPropagation();
-  const element = document.getElementById("thinkify_modal");
-  element.classList.add("menu-open");
-  var iframe = document.createElement("iframe");
+  const element = document.getElementById('thinkify_modal');
+  element.classList.add('menu-open');
+  var iframe = document.createElement('iframe');
   // iframe.setAttribute("scrolling", "no");
-  document.getElementById("iframe-wrapper").appendChild(iframe);
-  const model = $(".application-modal-wrap");
+  document.getElementById('iframe-wrapper').appendChild(iframe);
+  const model = $('.application-modal-wrap');
   const userData = getContentJson(model);
 
   if (userData) {
     const response = await getDetailsByAny(userData);
-    let url = "";
+    let url = '';
     const params = createObjectParams(userData);
-    console.log("====", response);
-    if (response.message === "User not found") {
-      url = `https://sleepy-meadow-81233.herokuapp.com/add?${params}`;
+    console.log('====', response);
+    if (response.message === 'User not found') {
+      url = `${URL}/add?${params}`;
     } else {
-      url = `https://sleepy-meadow-81233.herokuapp.com/?find=${response?.candidate?.email}&hf=true`;
+      url = `${URL}/?find=${response?.candidate?.email}&hf=true`;
     }
     iframe.src = url;
-    $("#modal__close").click(closeSidebar);
+    $('#modal__close').click(closeSidebar);
   }
 }
 
 function closeSidebar() {
-  const element = document.getElementById("thinkify_modal");
-  element.classList.remove("menu-open");
-  $("#iframe-wrapper").html("");
+  const element = document.getElementById('thinkify_modal');
+  element.classList.remove('menu-open');
+  $('#iframe-wrapper').html('');
 }
 
 function addButtonIfCorrectPage() {
-  console.log("====in buton ciode");
-  $(".candidate-action-links.resume-modal > div:nth-child(" + 3 + ")").after(
+  console.log('====in buton ciode');
+  $('.candidate-action-links.resume-modal > div:nth-child(' + 3 + ')').after(
     button
   );
 
   setTimeout(() => {
-    $("body").append(popupSidebar);
+    $('body').append(popupSidebar);
     document
-      .getElementById("thinkify-button")
-      .addEventListener("click", openSidebar);
+      .getElementById('thinkify-button')
+      .addEventListener('click', openSidebar);
     document
-      .getElementById("body-overlay")
-      .addEventListener("click", closeSidebar);
+      .getElementById('body-overlay')
+      .addEventListener('click', closeSidebar);
   }, 0);
 }
 
 // A $( document ).ready() block.
 $(document).ready(function () {
-  console.log("ready!");
-  $(".button-download-resume").click(function (e) {
+  console.log('ready!');
+  $('.button-download-resume').click(function (e) {
     e.stopPropagation();
     const id = setInterval(function () {
-      if ($(".application-modal-wrap").length) {
+      if ($('.application-modal-wrap').length) {
         addButtonIfCorrectPage();
         clearInterval(id);
       }

7c4db53267132a3634ac9cc0f42ed490b213542b
ashwin-thinkify <ak@thinkify.io>
2022-04-26 17:58:38 +0530

    updated the reloading issue for button

--- chrome-extension-candidate/linkedinForeground.js
+++ chrome-extension-candidate/linkedinForeground.js
@@ -1,35 +1,33 @@
 let instance = null;
 class Candidate {
-	constructor(name='',linkedInProfile='',data=null) {
-  
-	  if(!instance){
-		instance = this;
-	  }
+  constructor(name = "", linkedInProfile = "", data = null) {
+    if (!instance) {
+      instance = this;
+    }
 
-	  this.name = name;
-	  this.linkedInProfile = linkedInProfile;
-	  this.data = data;
-  
-	  return instance;
-	}
-  
-	getDetais() {
-	  return {
-		name:this.name,
-		linkedInProfile:this.linkedInProfile,
-		data: this.data,
-	  }
-	}
+    this.name = name;
+    this.linkedInProfile = linkedInProfile;
+    this.data = data;
 
-	setDetails({name,linkedInProfile,data}){
-		if(name)this.name = this.name;
-		if(linkedInProfile)this.linkedInProfile = linkedInProfile;
-		if(data)this.data=data;
-	}
+    return instance;
+  }
+
+  getDetais() {
+    return {
+      name: this.name,
+      linkedInProfile: this.linkedInProfile,
+      data: this.data,
+    };
+  }
+
+  setDetails({ name, linkedInProfile, data }) {
+    if (name) this.name = this.name;
+    if (linkedInProfile) this.linkedInProfile = linkedInProfile;
+    if (data) this.data = data;
+  }
 }
 
 let candidateInfo = new Candidate();
-
 
 const vetButton = `<a href="https://triplebyte.com/wt/thinkify/start/1Pq2hNl8qni/71104" target="_blank"><div class="vet-button">Vet Yourself</div></a>`;
 
@@ -41,30 +39,28 @@ const mainPopup = `<div id="demo-modal-popup" class="modal">
 	</div>
 </div>`;
 
-const routeToJoin = '/apply'
-
+const routeToJoin = "/apply";
 
 function showTestResults(e = null) {
   if (e) {
     e.stopPropagation();
   }
   var iframe = document.createElement("iframe");
-  let { linkedInProfile,name } = getDetailsOfCandidate();
+  let { linkedInProfile, name } = getDetailsOfCandidate();
   document.getElementById("iframe-wrapper").appendChild(iframe);
-  console.log('candidate:',candidateInfo.getDetais())
-  let {data} = candidateInfo.getDetais();
+  console.log("candidate:", candidateInfo.getDetais());
+  let { data } = candidateInfo.getDetais();
   debugger;
-  if(data?.linkedInProfile && data?.testResult){
-	iframe.src = `http://localhost:3000/?linkedInProfile=${data?.linkedInProfile}`;
-  } else if(data?.linkedInProfile){
-	iframe.src = `http://localhost:3000/take-test`;
+  if (data?.linkedInProfile && data?.testResult) {
+    iframe.src = `http://localhost:3000/?linkedInProfile=${data?.linkedInProfile}`;
+  } else if (data?.linkedInProfile) {
+    iframe.src = `http://localhost:3000/take-test`;
   } else {
-	iframe.src = `http://localhost:3000/apply/?linkedInProfile=${linkedInProfile}&name=${name}`;
+    iframe.src = `http://localhost:3000/apply/?linkedInProfile=${linkedInProfile}&name=${name}`;
   }
 }
 
 const getDetailsOfCandidate = () => {
-
   const contentExtracted = JSON.parse(
     $('code:contains("publicIdentifier")')[0].textContent
   );
@@ -74,7 +70,7 @@ const getDetailsOfCandidate = () => {
   const lastName = contentExtracted?.included[0]?.lastName || "";
   const name = firstName + " " + lastName;
 
-  candidateInfo.setDetails((linkedInProfile,name));
+  candidateInfo.setDetails((linkedInProfile, name));
 
   return {
     linkedInProfile,
@@ -91,8 +87,8 @@ function showMainPopupFirst() {
 
 function hideMainPopupFirst() {
   $("#demo-modal-popup").removeClass("modal__target");
-  $('#iframe-wrapper').html('');
-};
+  $("#iframe-wrapper").html("");
+}
 
 const getContentJson = async (linkedInProfile) => {
   const data = await getDetailsByLinkedInId(linkedInProfile);
@@ -100,25 +96,24 @@ const getContentJson = async (linkedInProfile) => {
 };
 
 const getButtonName = (candidate) => {
-   
-	let type = 1;
+  let type = 1;
 
-	if(candidate?.email && candidate?.testResult){
-		type = 3;
-	} else if(candidate?.email){
-		type = 2;
-	}
+  if (candidate?.email && candidate?.testResult) {
+    type = 3;
+  } else if (candidate?.email) {
+    type = 2;
+  }
 
-	const buttonName = {
-		1:'Add details',
-		2:'Vet yourself',
-		3:'Next Step'
-	}
-	const addDetailsButton = `<div class="add-details" id="demo-modal"><button>${buttonName[type]}</button></div>`;
-	return addDetailsButton;
-}
+  const buttonName = {
+    1: "Add details",
+    2: "Vet yourself",
+    3: "Next Step",
+  };
+  const addDetailsButton = `<div class="add-details" id="demo-modal"><button>${buttonName[type]}</button></div>`;
+  return addDetailsButton;
+};
 
-function addButtonIfCorrectPage (){
+function addButtonIfCorrectPage() {
   const { linkedInProfile } = getDetailsOfCandidate();
 
   var checkExist = setInterval(function () {
@@ -134,24 +129,39 @@ function addButtonIfCorrectPage (){
 
       getContentJson(linkedInProfile).then((candidate) => {
         console.log("candidate:", candidate);
-		candidateInfo.setDetails({data:candidate});
-		const buttonToShow = getButtonName(candidate);
-        appendButton(model, buttonToShow + mainPopup);
-        setTimeout(() => {
-          $("#demo-modal").click(showMainPopupFirst);
-        }, 0);
-
+        if ($("#demo-modal").length === 0) {
+          candidateInfo.setDetails({ data: candidate });
+          const buttonToShow = getButtonName(candidate);
+          appendButton(model, buttonToShow + mainPopup);
+          setTimeout(() => {
+            $("#demo-modal").click(showMainPopupFirst);
+          }, 0);
+        }
       });
-
       clearInterval(checkExist);
     }
   }, 100);
 }
 
+function onUrlChange() {
+  console.log("step2:");
+  setTimeout(() => {
+    addButtonIfCorrectPage();
+  }, 1000);
+}
+
 $(document).ready(function () {
-  console.log("ready!");
-  window.addEventListener('locationchange', function(){
-    console.log('location changed!');
-})
-  addButtonIfCorrectPage();
+  console.log("step1:");
+  onUrlChange();
+
+  let lastUrl = location.href;
+  new MutationObserver(() => {
+    const url = location.href;
+    if (url !== lastUrl) {
+      lastUrl = url;
+      if ($("#demo-modal").length === 0) {
+        onUrlChange();
+      }
+    }
+  }).observe(document, { subtree: true, childList: true });
 });
